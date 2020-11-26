e196c5029a00f0f1e6d707f3458b52fe3ff0803a
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-11-26 17:42:27 -0300

    feat: Implementation of the guess comparisons
    By now, after hitting the check button, the game will display a hint
    message to tell us if the guess is correct, lower or higher than the
    hidden number.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 'use strict';
 
+const secretNumber = 1 + Math.trunc(Math.random() * 20);
+
+
 document.querySelector('.check').addEventListener('click', function(){
 	const guess = Number(document.querySelector('.guess').value);
 	if(!guess){
 		document.querySelector('.message').textContent = 'No number';
+	}else if(guess === secretNumber){
+		document.querySelector('.message').textContent = 'Correct Number';	
+	}else if(guess > secretNumber){
+		document.querySelector('.message').textContent = 'Too hight';
+	}
+	else{
+		document.querySelector('.message').textContent = 'Too low';
 	}
 })
 
