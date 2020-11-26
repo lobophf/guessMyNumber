123677ab59e3ebddc6af8fdf3a23dfe557780b7d
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-11-26 17:58:00 -0300

    feat: Update score when tossing a wrong guess
    When the player toss a wrong guess the score must be decreased by 1.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const secretNumber = 1 + Math.trunc(Math.random() * 20);
+let score = 20;
 
+console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function(){
 	const guess = Number(document.querySelector('.guess').value);
@@ -11,9 +13,13 @@ document.querySelector('.check').addEventListener('click', function(){
 		document.querySelector('.message').textContent = 'Correct Number';	
 	}else if(guess > secretNumber){
 		document.querySelector('.message').textContent = 'Too hight';
+		score--;
+		document.querySelector('.score').textContent = score;
 	}
 	else{
 		document.querySelector('.message').textContent = 'Too low';
+		score--;
+		document.querySelector('.score').textContent = score;
 	}
 })
 
