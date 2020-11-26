0a1fdbf9208ee89535900db68bd5ae9c3cc4ecdd
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-11-26 18:18:14 -0300

    feat: Checking if the player loses
    The player will lose when the score hits zero.

--- script.js
+++ script.js
@@ -6,20 +6,26 @@ let score = 20;
 console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function(){
+
 	const guess = Number(document.querySelector('.guess').value);
-	if(!guess){
-		document.querySelector('.message').textContent = 'No number';
-	}else if(guess === secretNumber){
-		document.querySelector('.message').textContent = 'Correct Number';	
-	}else if(guess > secretNumber){
-		document.querySelector('.message').textContent = 'Too hight';
-		score--;
-		document.querySelector('.score').textContent = score;
+	if(!score){
+		document.querySelector('.message').textContent = 'You loose!';
 	}
 	else{
-		document.querySelector('.message').textContent = 'Too low';
-		score--;
-		document.querySelector('.score').textContent = score;
+		if(!guess){
+			document.querySelector('.message').textContent = 'No number';
+		}else if(guess === secretNumber){
+			document.querySelector('.message').textContent = 'Correct Number';	
+		}else if(guess > secretNumber){
+			document.querySelector('.message').textContent = 'Too hight';
+			score--;
+			document.querySelector('.score').textContent = score;
+		}
+		else{
+			document.querySelector('.message').textContent = 'Too low';
+			score--;
+			document.querySelector('.score').textContent = score;
+		}
 	}
 })
 
